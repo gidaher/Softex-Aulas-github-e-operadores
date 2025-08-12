085d7ecd8c37563f5adea98da2b8aa80ccd91bbe
gidaher <gi.techeducacao@gmail.com>
2025-08-12 15:53:34 -0300

    Primeiro commit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+
+// MÉDIA
+
 let n1 = 7
 let n2 = 5
 let n3 = 10
@@ -5,3 +8,29 @@ let n4 = 10
 
 let media = (n1+n2+n3+n4)/4
 console.log(media)
+
+
+
+let numero = Math.sqrt(9)
+console.log(numero)
+
+// FÓRMULA DE BHASKARA 
+
+
+let a = 19;
+let b = 20;
+let c = 30;
+
+let delta = ((b*b)(4*a*c));
+
+let raiz = Math.sqrt(delta)
+
+let x1 = (-b + raiz)/ (2*a);
+let x2 = (-b - raiz)/ (2*a);
+
+console.log (raiz)
+console.log(x1)
+console.log(x2)
+
+
+
